86f341a849012d0b07c30421a650cabbb4771190
Ava Matias <matias.ava_eb@hotmail.com>
2023-01-14 16:31:49 -0300

    orden de html y js

--- js/CV.js
+++ js/CV.js
@@ -1,113 +1,119 @@
 
-function seleccion (){
-document.getElementById("op1").classList.add("b1");
-document.getElementById("op3").classList.add("b1");
-document.getElementById("op4").classList.add("b1");
-document.getElementById("op5").classList.add("b1");
-document.getElementById("op6").classList.add("b1");
-document.getElementById("op7").classList.add("b1");
-document.getElementById("op2").classList.remove("b1");
+function seleccion ()
+ {
+   document.getElementById("op1").classList.add("b1");
+   document.getElementById("op3").classList.add("b1");
+   document.getElementById("op4").classList.add("b1");
+   document.getElementById("op5").classList.add("b1");
+   document.getElementById("op6").classList.add("b1");
+   document.getElementById("op7").classList.add("b1");
+   document.getElementById("op2").classList.remove("b1");
 
+ }
 
-}
-
-document.getElementById("Pp").onclick = function () {
+ document.getElementById("Pp").onclick = function () 
+  {
     seleccion();
-}
-function seleccion1 (){
+  }
+    function seleccion1 ()
+     {
 
-document.getElementById("op1").classList.add("b1");
-document.getElementById("op2").classList.add("b1");
-document.getElementById("op4").classList.add("b1");
-document.getElementById("op5").classList.add("b1");
-document.getElementById("op6").classList.add("b1");
-document.getElementById("op7").classList.add("b1");
-document.getElementById("op3").classList.remove("b1");
+      document.getElementById("op1").classList.add("b1");
+      document.getElementById("op2").classList.add("b1");
+      document.getElementById("op4").classList.add("b1");
+      document.getElementById("op5").classList.add("b1");
+      document.getElementById("op6").classList.add("b1");
+      document.getElementById("op7").classList.add("b1");
+      document.getElementById("op3").classList.remove("b1");
+     
+     }
     
-    
-    
-    }
-    document.getElementById("Ip").onclick = function () {
+     document.getElementById("Ip").onclick = function ()
+      {
         seleccion1();
-    }
+      }
 
-    function seleccion2 (){
+        function seleccion2 ()
+         {
 
-        document.getElementById("op1").classList.add("b1");
-        document.getElementById("op2").classList.add("b1");
-        document.getElementById("op3").classList.add("b1");
-        document.getElementById("op5").classList.add("b1");
-        document.getElementById("op6").classList.add("b1");
-        document.getElementById("op7").classList.add("b1");
-        document.getElementById("op4").classList.remove("b1");
-            
-            
-            
-            }
-            document.getElementById("Ca").onclick = function () {
-                seleccion2();
-            }
-            function seleccion3 (){
+          document.getElementById("op1").classList.add("b1");
+          document.getElementById("op2").classList.add("b1");
+          document.getElementById("op3").classList.add("b1");
+          document.getElementById("op5").classList.add("b1");
+          document.getElementById("op6").classList.add("b1");
+          document.getElementById("op7").classList.add("b1");
+          document.getElementById("op4").classList.remove("b1");
+         }
+         
+         document.getElementById("Ca").onclick = function () 
+          {
+            seleccion2();
+          }
+            function seleccion3 ()
+             {
 
-                document.getElementById("op1").classList.add("b1");
-                document.getElementById("op2").classList.add("b1");
-                document.getElementById("op4").classList.add("b1");
-                document.getElementById("op3").classList.add("b1");
-                document.getElementById("op6").classList.add("b1");
-                document.getElementById("op7").classList.add("b1");
-                document.getElementById("op5").classList.remove("b1");
-                    
-                    
-                    
-                    }
-                    document.getElementById("Cu").onclick = function () {
-                        seleccion3();
-                    }
-                    function seleccion4 (){
+              document.getElementById("op1").classList.add("b1");
+              document.getElementById("op2").classList.add("b1");
+              document.getElementById("op4").classList.add("b1");
+              document.getElementById("op3").classList.add("b1");
+              document.getElementById("op6").classList.add("b1");
+              document.getElementById("op7").classList.add("b1");
+              document.getElementById("op5").classList.remove("b1");
+             
+             }
+              
+             document.getElementById("Cu").onclick = function ()
+              {
+                seleccion3();
+              }
+                function seleccion4 ()
+                 {
 
-                        document.getElementById("op1").classList.add("b1");
-                        document.getElementById("op2").classList.add("b1");
-                        document.getElementById("op4").classList.add("b1");
-                        document.getElementById("op5").classList.add("b1");
-                        document.getElementById("op3").classList.add("b1");
-                        document.getElementById("op7").classList.add("b1");
-                        document.getElementById("op6").classList.remove("b1");
+                  document.getElementById("op1").classList.add("b1");
+                  document.getElementById("op2").classList.add("b1");
+                  document.getElementById("op4").classList.add("b1");
+                  document.getElementById("op5").classList.add("b1");
+                  document.getElementById("op3").classList.add("b1");
+                  document.getElementById("op7").classList.add("b1");
+                  document.getElementById("op6").classList.remove("b1");
                             
-                            
-                            
-                            }
-                            document.getElementById("Ex").onclick = function () {
-                                seleccion4();
-                            }
-                            function seleccion5 (){
+                 }
+                  
+                 document.getElementById("Ex").onclick = function () 
+                  {
+                    seleccion4();
+                  }
+                    function seleccion5 ()
+                     {
 
-                                document.getElementById("op1").classList.add("b1");
-                                document.getElementById("op2").classList.add("b1");
-                                document.getElementById("op4").classList.add("b1");
-                                document.getElementById("op5").classList.add("b1");
-                                document.getElementById("op6").classList.add("b1");
-                                document.getElementById("op3").classList.add("b1");
-                                document.getElementById("op7").classList.remove("b1");
-                                    
-                                    
-                                    
-                                    }
-                                    document.getElementById("Co").onclick = function () {
-                                        seleccion5();
-                                    }
-                                    function seleccion6 (){
+                       document.getElementById("op1").classList.add("b1");
+                       document.getElementById("op2").classList.add("b1");
+                       document.getElementById("op4").classList.add("b1");
+                       document.getElementById("op5").classList.add("b1");
+                       document.getElementById("op6").classList.add("b1");
+                       document.getElementById("op3").classList.add("b1");
+                       document.getElementById("op7").classList.remove("b1");
+                     
+                     }
+                     
+                     document.getElementById("Co").onclick = function () 
+                      {
+                        seleccion5();
+                      }
+                        function seleccion6 ()
+                         {
 
-                                        document.getElementById("op3").classList.add("b1");
-                                        document.getElementById("op2").classList.add("b1");
-                                        document.getElementById("op4").classList.add("b1");
-                                        document.getElementById("op5").classList.add("b1");
-                                        document.getElementById("op6").classList.add("b1");
-                                        document.getElementById("op7").classList.add("b1");
-                                        document.getElementById("op1").classList.remove("b1");
+                           document.getElementById("op3").classList.add("b1");
+                           document.getElementById("op2").classList.add("b1");
+                           document.getElementById("op4").classList.add("b1");
+                           document.getElementById("op5").classList.add("b1");
+                           document.getElementById("op6").classList.add("b1");
+                           document.getElementById("op7").classList.add("b1");
+                           document.getElementById("op1").classList.remove("b1");
                                             
-                                            
-                                            
-                                            }
-                                            document.getElementById("In").onclick = function () {
-                                                seleccion6();
-                                            }
+                         }
+                         
+                         document.getElementById("In").onclick = function () 
+                          {
+                            seleccion6();
+                          }
